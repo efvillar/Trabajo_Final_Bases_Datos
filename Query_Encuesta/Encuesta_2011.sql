bf19d7b5dabcb1127d0e186841161be1a144866f
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-06-11 09:54:14 -0500

    Add files via upload

---
+++ Query_Encuesta/Encuesta_2011.sql
@@ -0,0 +1,23 @@
+SELECT 
+2011 as Year,
+string_field_0 as Id_Country,
+string_field_3 as Id_Experience,
+string_field_6 as Id_Ocupation,
+string_field_45 as Id_Salary,
+string_field_2 as Id_Age,
+null as Id_Gender,
+string_field_39 as C,
+string_field_38 as Cplusplus,
+string_field_37 as CSharp,
+string_field_32 as CSS,
+string_field_30 as Java,
+string_field_31 as JavaScript,
+string_field_40 as Perl,
+string_field_33 as PHP,
+string_field_34 as Python,
+string_field_35 as Ruby,
+string_field_36 as SQL,
+string_field_41 as Ninguno,
+string_field_42 as Otros
+
+FROM `fh-bigquery.stackoverflow.survey_results_2011` 
