e154cb2dd65ff9142bbbc7e23c6a6ddb6700e45f
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-06-11 16:42:18 -0500

    Update Encuesta_2013.sql

--- Query_Encuesta/Encuesta_2013.sql
+++ Query_Encuesta/Encuesta_2013.sql
@@ -5,10 +5,19 @@ string_field_3 as Id_Experience,
 string_field_6 as Id_Ocupation,
 string_field_100 as Id_Salary,
 string_field_2 as Id_Age,
-null as Gender,
+null as Id_Gender,
+(case when string_field_69 like '%AngularJS%' then 'AngularJS'
+else null
+end) as AngularJS,
 string_field_56 as C,
 string_field_57 as Cplusplus,
 string_field_58 as CSharp,
+(case when string_field_69 like '%CSS%' then 'CSS'
+else null
+end) as CSS,
+(case when string_field_69 like '%HTML5%' then 'HTML5'
+else null
+end) as HTML5,
 string_field_59 as Java,
 string_field_60 as JavaScript,
 (Case when string_field_61 is not null or string_field_62 is not null then 'jQuery'
@@ -16,10 +25,18 @@ else null
 end) as jQuery,
 string_field_63 as Nodejs,
 string_field_64 as ObjectiveC,
+(case when string_field_69 like '%Perl%' then 'Perl'
+else null
+end) as Perl,
 string_field_65 as PHP,
 string_field_66 as Python,
 string_field_67 as Ruby,
 string_field_68 as SQL,
-string_field_69 as Otros
+(case when string_field_69 like '%SQL_Server%' then 'SQL_Server'
+else null
+end) as SQL_Server,
+(case when string_field_69 like '%TypeScript%' then 'TypeScript'
+else null
+end) as TypeScript
 
 FROM `fh-bigquery.stackoverflow.survey_results_2013` 
