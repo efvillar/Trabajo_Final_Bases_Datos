7db739a059716007b285f992de58c96bae054b6e
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-06-17 18:59:46 -0500

    aa
    a

---
+++ Query_Encuesta/Validacion_join_tablas.sql
@@ -0,0 +1,112 @@
+--AGE
+
+SELECT Distinct
+enc.Year,
+enc.Id_Age,
+age.Age,
+age.Desc_Age
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Age_v0` age
+on (enc.Year=age.Year and enc.Id_Age=age.Id_Age)
+
+--Gender
+
+SELECT Distinct
+enc.Year,
+enc.Id_Gender,
+gen.Gender,
+gen.Desc_Gender
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Gender_v0` gen
+on (enc.Year=gen.Year and enc.Id_Gender=gen.Id_Gender)
+
+--Country
+
+SELECT Distinct
+enc.Year,
+enc.Id_Country,
+con.Country,
+con.Des_Country
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Country_v0` con
+on (enc.Year=con.Year and enc.Id_Country=con.Id_Country)
+
+
+--Experience
+
+SELECT Distinct
+enc.Year,
+enc.Id_Experience,
+exp.Experience,
+exp.Desc_Experience
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Experience_v0` exp
+on (enc.Year=exp.Year and enc.Id_Experience=exp.Id_Experience)
+
+
+--Salary 
+
+SELECT Distinct
+enc.Year,
+enc.Id_Salary,
+sal.Salary,
+sal.Desc_Salary
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Salary_v1` sal
+on (enc.Year=sal.Year and enc.Id_Salary=sal.Id_Salary)
+
+
+
+--- Encuesta
+
+
+Create table `basededatosmasivos.Base_de_Datos.Encuestas_v1`
+as
+SELECT 
+enc.Year,
+con.Country,
+exp.Experience,
+enc.Id_Ocupation,
+sal.Salary,
+age.Age,
+gen.Gender,
+enc.AngularJS,
+enc.C,
+enc.CPlusPlus,
+enc.CSharp,
+enc.CSS,
+enc.HTML5,
+enc.Java,
+enc.JavaScript,
+enc.jQuery,
+enc.Nodejs,
+enc.ObjectiveC,
+enc.Perl,
+enc.PHP,
+enc.Python,
+enc.Ruby,
+enc.SQL,
+enc.SQL_Server,
+enc.TypeScript
+
+FROM `basededatosmasivos.Base_de_Datos.Encuestas` enc
+
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Age_v0` age
+on (enc.Year=age.Year and enc.Id_Age=age.Id_Age)
+
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Gender_v0` gen
+on (enc.Year=gen.Year and enc.Id_Gender=gen.Id_Gender)
+
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Country_v0` con
+on (enc.Year=con.Year and enc.Id_Country=con.Id_Country)
+
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Experience_v0` exp
+on (enc.Year=exp.Year and enc.Id_Experience=exp.Id_Experience)
+
+LEFT JOIN `basededatosmasivos.Base_de_Datos.Salary_v1` sal
+on (enc.Year=sal.Year and enc.Id_Salary=sal.Id_Salary)
